04556b9a0bb264e9b97f468ae9f73d9ea9bf050c
Netanel Mazuz <netazuz@gmail.com>
2021-04-27 22:47:58 +0300

    update

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,38 +1,35 @@
 import React from "react";
-
 import "./Search.css";
 
 function Search(props) {
   return (
     <div className="searchContainer">
-      <div className="searchBox">
-        <label className="searchLabel searchText">Enter url : </label>
-        <br></br>
-        <input
-          className="search"
-          onChange={(e) => props.search(e.target.value)}
-          name="name"
-          value={props.data}
-          type="text"
-        />
-      </div>
-      <div className="langBox">
-        <span className="searchText">Choose a language : </span>
-        <select
-          className="select"
-          onChange={(e) => props.lang(e.target.value)}
-          name="lang"
-          id="lang"
-        >
-          <option value="english">English</option>
-          <option value="arabic">عرب</option>
-          <option value="russian">Русский</option>
-          <option value="hebrew">עברית</option>
-        </select>
-        <div className="sendBtn" onClick={() => props.send()}>
-          Translate
+      <div className="search-wrapper">
+
+        <div className="searchBox">
+          <label className="searchLabel searchText">Enter url: </label>
+          <br></br>
+          <input
+            className="search"
+            onChange={(e) => props.search(e.target.value)}
+            name="name"
+            value={props.data}
+            type="text"
+          />
         </div>
-        <p>Choosed language: {props.dataLang}</p>
+        <div className="langBox">
+          <span className="searchText">Translate to:
+            <select className="select" onChange={(e) => props.lang(e.target.value)} name="lang" id="lang" >
+              <option value="english">English</option>
+              <option value="arabic">عرب</option>
+              <option value="russian">Русский</option>
+              <option value="hebrew">עברית</option>
+            </select>
+          </span>
+          <butto className="sendBtn" onClick={() => props.send()}>
+            Translate
+          </butto>
+        </div>
       </div>
     </div>
   );
