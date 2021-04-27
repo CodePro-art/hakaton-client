0ef43cb52da89a2a86fc5b18d9e90fef97961b9c
Miryam Oren <miriam839@gmail.com>
2021-04-28 01:11:31 +0300

    commit

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,17 +2,32 @@ import React, { useState } from "react";
 import "./Display.css";
 
 function Display(props) {
+  
   // page open - props === null;
   // story loding - props === 'loading'
   // get story data - props === obj
-  console.log(props.data)
+  console.log(props.data);
+  const renderHeader = () => {
+    const align = ['en','ru'].includes(props.data.language)? 'left': 'right'
+    return (
+      <div className="display_header">
+        {align === 'left' ?
+          <React.Fragment>
+            <h2 className="display_title">{props.data.title}</h2>
+            <img className="display_img" src={props.data.image}/>
+          </React.Fragment> :
+          <React.Fragment>
+            <img className="display_img" src={props.data.image}/> 
+            <h2 className="display_title">{props.data.title}</h2>
+          </React.Fragment>
+        }
+      </div>
+    );
+  }
   const renderContent = () => {
       return (
         <React.Fragment>
-          <div className="display_header">
-          <h2 className="display_title">{props.data.title}</h2>
-          <img className="display_img" src={props.data.image}/>
-          </div>
+          
           <div className="display_txt">
             {props.data.content.map((p, indx) => <p key={indx}>{p}</p>)}
           </div>
