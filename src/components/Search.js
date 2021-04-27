08ef2602cf3f710473720429333e49e247571ec2
Yakov Gurevich <umanrh@gmail.com>
2021-04-27 18:04:11 +0300

    some updates search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,21 +5,32 @@ import "./Search.css";
 function Search(props) {
   return (
     <>
-      <div className="searchBox">
-        <label>Enter url :</label>
-        <br></br>
-        <input
-          className="search"
-          onChange={(e) => props.search(e.target.value)}
-          name="name"
-          value={props.data}
-          type="text"
-        />
-        <div className="sendBtn" onClick={() => props.send()}>
-          Translate
+      <div className="searchContainer">
+        <div className="searchBox">
+          <label className="searchLabel searchText">Enter url : </label>
+          <br></br>
+          <input
+            className="search"
+            onChange={(e) => props.search(e.target.value)}
+            name="name"
+            value={props.data}
+            type="text"
+          />
         </div>
+        <div className="langBox">
+          <span className="searchText">Choose a language : </span>
+          <select className="select" name="lang" id="lang">
+            <option value="english">English</option>
+            <option value="arabic">Arabic</option>
+            <option value="russion">Russion</option>
+            <option value="hebrew">Hebrew</option>
+          </select>
+          <div className="sendBtn" onClick={() => props.send()}>
+            Translate
+          </div>
+        </div>
+        <p>{props.data}</p>
       </div>
-      <p>{props.data}</p>
     </>
   );
 }
