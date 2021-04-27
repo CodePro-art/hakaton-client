3b68df61e767eb7f387e40a834b59df2adc31b1a
Miryam Oren <miriam839@gmail.com>
2021-04-27 18:47:54 +0300

    commit

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
 import "./Display.css";
 
-function Display() {
+function Display(props) {
+  console.log(props.data)
   // { title, imgSrc, content}
-  const title = 'Long Long Long Title';
-  const imgSrc = 'https://www.adamtsair.co.il/wp-content/uploads/2018/09/crazy.jpg';
-  const content = `React checks the props passed to components against those definitions,
-   and will result in a warning/error when they don't match. We can define default values
-    to props using the defaultProps property. defaultProps is used to ensure that props will
-     have a value if it was not specified by the parent component. there is a alternative to
-      margin-left:auto; margin-right: auto; or margin:0 auto; for the ones that use position:
-      absolute; this is how: you set the left position of the element to 50% (left:50%;) but
-       that will not center it correctly in order for the element to be centered correctly you
-        need to give it a margin of minus half of its width, that will center your element
-         perfectly here is an example: http://jsfiddle.net/35ERq/3/`;
+  // const title = 'Long Long Long Title';
+  // const imgSrc = 'https://www.adamtsair.co.il/wp-content/uploads/2018/09/crazy.jpg';
+  // const content = `React checks the props passed to components against those definitions,
+  //  and will result in a warning/error when they don't match. We can define default values
+  //   to props using the defaultProps property. defaultProps is used to ensure that props will
+  //    have a value if it was not specified by the parent component. there is a alternative to
+  //     margin-left:auto; margin-right: auto; or margin:0 auto; for the ones that use position:
+  //     absolute; this is how: you set the left position of the element to 50% (left:50%;) but
+  //      that will not center it correctly in order for the element to be centered correctly you
+  //       need to give it a margin of minus half of its width, that will center your element
+  //        perfectly here is an example: http://jsfiddle.net/35ERq/3/`;
   return (
     <div className="display">
       <div className="display_header">
-        <h2 className="display_title">{title}</h2>
-        <img className="display_img" src={imgSrc}/>
+        <h2 className="display_title">{props.data.title}</h2>
+        <img className="display_img" src={props.data.image}/>
       </div>
-      <p className="display_txt">{content}</p>
+      <p className="display_txt">{props.data.content}</p>
     </div>
   );
 }
